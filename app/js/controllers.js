0d658d30208f54c0c6ad217211799e95cdbe366b
Collin Glass <collinglass@gmx.com>
2013-11-28 23:01:02 -0500

    Admit patient

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,14 +12,43 @@ angular.module('mustacheApp.controllers', []).
         // Doing nothing
 
     }]).controller('WardDetailCtrl', ["$scope", function ($scope) {
+        $scope.getTotalPatients = function() {
+            return $scope.patients.length;
+        };
+
+        $scope.getTotalAdmissions = function() {
+            return $scope.admissions.length;
+        };
+
+       $scope.admitPatient = function() {
+            angular.forEach($scope.admissions, function(obj) {
+                if ( obj.selected == true ) {
+                    $scope.patients.push({lastName: obj.lastName, firstName: obj.firstName, healthInsNum: obj.healthInsNum, roomNum: "00", bedNum: "00", status: "nominal",});
+                    $scope.admissions.splice(obj, 1);
+                }
+            });
+
+        };
+
         $scope.patients = [
             {
                 lastName: "Smith",
                 firstName: "John",
-                heathInsNum: "0123 4567 89AB CDEF",
+                healthInsNum: "0123 4567 89AB CDEF",
                 roomNum: 9000,
                 bedNum: 42,
                 status: "nominal"
+            }
+        ];
+        $scope.admissions = [
+            {
+                lastName: "Balls",
+                firstName: "Harry",
+                healthInsNum: "9876 5432 10FE DCBA",
+                outboundWard: 9001,
+                outboundNurse: 41,
+                priority: "nominal",
+                rationale: "The patient..."
             }
         ];
 
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,12 @@
 
 /* Filters */
 
-angular.module('mustacheApp.filters', []).
+var mustacheFilters = angular.module('mustacheApp.filters', []);
+
+mustacheFilters.
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
   }]);
+
