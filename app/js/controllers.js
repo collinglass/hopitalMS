d5e5fde3d0c1ad0183a116bcecbc93a8163b112f
Antoine Grondin <antoinegrondin@gmail.com>
2013-11-30 14:21:59 -0500

    Fix creation of methods on admissions/patients before they exist.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,21 +16,45 @@ controllers.controller('RegisterCtrl', [function () {
 controllers.controller('WardListCtrl', ["$scope", "Ward", function ($scope, Ward) {
         $scope.wards = Ward.query();
     }]).controller('WardDetailCtrl', ["$scope", "$routeParams", "Ward", function ($scope, $routeParams, Ward) {
-        Ward.get({wardId: $routeParams.wardId}, function(ward){
+        Ward.get({wardId: $routeParams.wardId}, function (ward) {
             $scope.ward = ward;
             $scope.patients = ward.patients;
             $scope.admissions = ward.admissions;
+
+            $scope.admisions.admit = function () {
+
+                angular.forEach($scope.admissions, function (patient) {
+                    if (!patient.selected) {
+                        // ignore those that aren't selected
+                        return;
+                    }
+                    $scope.patients.push({
+                        lastName: patient.lastName,
+                        firstName: patient.firstName,
+                        healthInsNum: patient.healthInsNum,
+                        roomNum: "00",
+                        bedNum: "00",
+                        status: "nominal"
+                    });
+
+                    $scope.admissions.splice(patient, 1);
+                });
+            };
+
+            $scope.patients.discharge = function () {
+                angular.forEach($scope.patients, function (patient) {
+                    if (!patient.selected) {
+                        // ignore those that aren't selected
+                        return;
+                    }
+                    var index = $scope.patients.indexOf(patient);
+                    $scope.patients.splice(index, 1);
+                });
+            };
+
         });
 
-        $scope.admitPatient = function() {
 
-            angular.forEach($scope.admissions, function(obj) {
-                if ( obj.selected === true ) {
-                    $scope.patients.push({lastName: obj.lastName, firstName: obj.firstName, healthInsNum: obj.healthInsNum, roomNum: "00", bedNum: "00", status: "nominal",});
-                    $scope.admissions.splice(obj, 1);
-                }
-            });
-        };
     }]);
 
 
