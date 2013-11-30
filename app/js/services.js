8364d1f3aa556567a7522d06c4f0cf1edc528229
Antoine Grondin <antoinegrondin@gmail.com>
2013-11-30 15:11:00 -0500

    Add service for Patient/User/NOK and stub, bad JSON for them.

--- app/js/services.js
+++ app/js/services.js
@@ -10,3 +10,21 @@ mustacheServices.factory("Ward", ["$resource", function($resource){
         query: {method:'GET', params:{wardId:'@id'}, isArray:true}
     });
 }]);
+
+mustacheServices.factory("Patient", ["$resource", function($resource){
+    return $resource('/api/v0.1/patients/:patientId', {patientId:'@id'}, {
+        query: {method:'GET', params:{patientId:'@id'}, isArray:true}
+    });
+}]);
+
+mustacheServices.factory("NextOfKin", ["$resource", function($resource){
+    return $resource('/api/v0.1/nextOfKins/:nokId', {nokId:'@id'}, {
+        query: {method:'GET', params:{nokId:'@id'}, isArray:true}
+    });
+}]);
+
+mustacheServices.factory("User", ["$resource", function($resource){
+    return $resource('/api/v0.1/users/:userId', {userId:'@id'}, {
+        query: {method:'GET', params:{userId:'@id'}, isArray:true}
+    });
+}]);
